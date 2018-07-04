8923adc108d5699182407d4953fa0510b8c64aea
kalug89 <kaluzny_david@hotmail.com>
2018-07-04 19:57:31 +0200

    Scope

---
+++ Centrum Programisty tutorial 01-2018/src/Scope.kt
@@ -0,0 +1,11 @@
+var name:String = "Mike Tyson"
+fun display():Unit{
+    println(name)
+}
+fun show():Unit{
+    var secondName:String = "Gienek"
+    println(name + "Second name: $secondName ")
+}
+fun main(args: Array<String>){
+  display()
+}
