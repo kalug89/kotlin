5ec070782d62a2f01a06ca98e5cbc068f66d905b
kalug89 <kaluzny_david@hotmail.com>
2018-07-04 20:07:55 +0200

    Object

---
+++ Centrum Programisty tutorial 01-2018/src/Object.kt
@@ -0,0 +1,14 @@
+samochód
+    typ: String = BMW
+    model:String = E36
+    Price:Int = 10000
+    Owner:String = "Sebix"
+
+    GetPrice
+    GetModel
+    GetType
+    GetOwner
+
+fun main(args: Array<String>){
+
+}
