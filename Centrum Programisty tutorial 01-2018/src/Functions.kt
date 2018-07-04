86976aea3abc7262f353c860c087e6dfa18dc6d0
kalug89 <kaluzny_david@hotmail.com>
2018-07-04 19:30:07 +0200

    Function kotlin

--- Centrum Programisty tutorial 01-2018/src/Functions.kt
+++ Centrum Programisty tutorial 01-2018/src/Functions.kt
@@ -4,8 +4,8 @@ fun sum(a:Double, b:Double):Double{
 fun multiply(a:Int, b:Int):Int{
     return a * b
 }
-fun display(n:Int){
-    println("n: $n")
+fun display(n:Int = 1){
+    println("$n")
 }
 fun displayWithnoUnit():Unit{
     println("Error")
@@ -14,6 +14,7 @@ fun displayWithnoUnit():Unit{
 fun main(args: Array<String>) {
     println(sum(2.5, 1.2))
     println(multiply(4,4))
-    display(44)
+    display()
+    display(55)
     displayWithnoUnit()
 }
