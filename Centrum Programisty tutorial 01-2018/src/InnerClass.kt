a72ac458532ea70980234407d7812dbad8db2d06
kalug89 <kaluzny_david@hotmail.com>
2018-07-06 22:43:34 +0200

    Inner class

---
+++ Centrum Programisty tutorial 01-2018/src/InnerClass.kt
@@ -0,0 +1,18 @@
+class Outer{
+    private val ageage:Int? = 12
+
+
+
+    inner class Nested{
+
+        fun Show(){
+            println(ageage)
+        }
+    }
+}
+
+//fun main(args: Array<String>){
+//    var outer = Outer()
+//    var nested = Outer.Nested()//Czemu to nie działa
+//    nested.Show()
+//}
