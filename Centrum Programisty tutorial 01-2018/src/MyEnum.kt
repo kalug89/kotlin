fd486376245f11a6571808802620350b646f1eb7
kalug89 <kaluzny_david@hotmail.com>
2018-07-06 22:09:54 +0200

    Enum

---
+++ Centrum Programisty tutorial 01-2018/src/MyEnum.kt
@@ -0,0 +1,15 @@
+enum class Tshirt(){
+    S,M,L,X,XL,XXL
+}
+
+fun main(args: Array<String>){
+    var size = Tshirt.S
+    
+
+    if(size == Tshirt.S){
+        println("Sise is minimum of possibility")
+    } else {
+        println("What ever you want")
+    }
+
+}
