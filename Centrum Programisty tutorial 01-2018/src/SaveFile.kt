ec76acfe415c8fca07d40ca0028e90004fb1722c
kalug89 <kaluzny_david@hotmail.com>
2018-07-07 13:00:25 +0200

    Load file

--- Centrum Programisty tutorial 01-2018/src/SaveFile.kt
+++ Centrum Programisty tutorial 01-2018/src/SaveFile.kt
@@ -1,17 +1,32 @@
+import java.io.FileReader
 import java.io.FileWriter
 
 fun main(args:Array<String>){
     println("Enter the full name")
     var name:String? = readLine()
     writeToFile(name!!)
+
+    readToFile()
+
+}
+
+fun readToFile() {
+    try {
+        var read = FileReader("mike.txt")
+        var a:Int?
+        do{
+            a = read.read()
+            print(a.toChar())
+        } while (a!=-1)
+    }catch (e:Exception){
+        println(e.message)
+    }
 }
 
 fun writeToFile(s: String){
 
     try{
         println("Enter name for file")
-
-
         var string:String? = readLine()
         var save = FileWriter(string +".txt")
         save.write(s)
