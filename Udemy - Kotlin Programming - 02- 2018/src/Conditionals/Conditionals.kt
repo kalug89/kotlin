b54adb0b589946450c3579af2749c0f637741c04
kalug89 <kaluzny_david@hotmail.com>
2018-07-08 20:22:39 +0200

    Conditionals

---
+++ Udemy - Kotlin Programming - 02- 2018/src/Conditionals/Conditionals.kt
@@ -0,0 +1,15 @@
+package Conditionals
+
+fun main(args: Array<String>) {
+    val age: Int = 29
+    if (age < 18) {
+        println("You cannot register")
+    } else if (age < 21) {
+        println("Well, maybe you can register")
+    } else if (age < 27) {
+        println("You should be able to register")
+    } else{
+        println("You can register")
+    }
+    println("The end")
+}
