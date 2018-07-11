717cb914a49da4e3d6917cf1affe8cd2946634e6
kalug89 <kaluzny_david@hotmail.com>
2018-07-11 18:22:53 +0200

    Array list

---
+++ Udemy - Kotlin Programming - 02- 2018/src/collections/ArrayList.kt
@@ -0,0 +1,9 @@
+package collections
+
+fun main(args: Array<String>){
+    val array = arrayOf("Texas", "Iowa", "California")
+    val list = listOf("Orange", "Apple", "Bananas")
+    val arrayList = arrayListOf("Patric", "Michael", "Sarach")
+    arrayList.add("Sandra")
+    println(arrayList)
+}
