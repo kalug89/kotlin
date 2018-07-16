b458f9fb837a3b5605c015b5fec878732baa986a
kalug89 <kaluzny_david@hotmail.com>
2018-07-16 18:53:12 +0200

    Stack

---
+++ Udemy - Kotlin Programming - 02- 2018/src/oo/Animal.kt
@@ -0,0 +1,24 @@
+package oo
+
+open class Animal {
+    private var age: Int = 0
+    protected var name = "Sam"
+    internal val isDangerous = true
+
+
+    public fun isOld(): Boolean{
+        return age > 12
+    }
+}
+
+class Vertabrate : Animal(){
+    fun introduceYourself(){
+        println(this.name)
+    }
+}
+
+
+fun main(args: Array<String>) {
+    val animal = Animal()
+
+}
---
+++ Udemy - Kotlin Programming - 02- 2018/src/oo/Stack.kt
@@ -0,0 +1,13 @@
+package oo
+
+//DRY = don't repeat yourself
+class Stack<E> {
+
+    fun push() {
+      
+    }
+
+    fun pop() {
+
+    }
+}
